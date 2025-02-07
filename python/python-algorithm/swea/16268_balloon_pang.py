e16c8d66822e0f028ff11249477d309f2669af70
jiwon <jiwnee252@gmail.com>
2025-02-07 17:11:31 +0900

    16268_풍선팡2

--- python/python-algorithm/swea/16268_balloon_pang.py
+++ python/python-algorithm/swea/16268_balloon_pang.py
@@ -2,20 +2,34 @@ import sys
 sys.stdin = open("16268.txt", "r")
 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    print(N)
-    print(M)
-    print(arr)
 
-    # 가로M 세로N
+    # arr[i][j]의 상하좌우 칸이 arr[ni][nj] 라고할때
+    # didj 방향별로더할값..
 
+    # 구하는것 -> arr[i][j] 의 값 + 상하좌우칸 총 5개칸의값의합
+    # arr[i][j]값을 기준으로
+    # 상 하 좌 우 (4번) 반복해서 arr[ni][nj]의 값을 더해줌
 
-    # dx = [0, 1, 0, 1]  # 시계방향 # 방향별로 더할 값
-    # dy = [1, 0, -1, 0]
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
 
-    for
+    # f_sum = 기준칸의 값
+    # max_sum = 최대합
 
-    # print(f'#{test_case} {result}')
+    max_sum = 0
+
+    for i in range(N):    # 0부터 N-1까지.
+        for j in range(M):
+            f_sum = arr[i][j]
+            for d in range(4):  # 4방향탐색
+                ni = i + di[d]   # i-1, i+1, i+0, i+0
+                nj = j + dj[d]   # j+0, j+0, j-1, j+1
+                if 0 <= ni < N and 0 <= nj < M:
+                    f_sum += arr[ni][nj]
+            if max_sum < f_sum:
+                max_sum = f_sum
+
+    print(f'#{test_case} {max_sum}')
