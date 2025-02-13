5fd22176bcc68f59cd58b4c01ffb3a991231c7b2
jiwon <jiwnee252@gmail.com>
2025-02-13 17:08:07 +0900

    비밀번호

--- python/python-algorithm/swea/1234_password.py
+++ python/python-algorithm/swea/1234_password.py
@@ -1,5 +1,23 @@
 import sys
 sys.stdin = open("1234.txt", "r")
 
-T = int(input())
-for test_case in range(1, T+1):
+for test_case in range(1, 11):
+    tc, num = input().split()
+    # print(num)
+
+    password = list(num)
+    print(password)
+    stack = []
+    for i in password:
+        # 스택에 아무것도 없으면 현재값을 추가
+        if len(stack) == 0:
+            stack.append(i)
+        # 스택에 머가잇으면..
+        else:
+            # 스택의 맨마지막숫자가 현재숫자와 다를경우
+            if stack[-1] != i:
+                stack.append(i)
+            # 스택의 맨마지막숫자가 현재숫자와 같을경우
+            else:
+                stack.pop()
+    print(f'#{test_case} {"".join(stack)}')
