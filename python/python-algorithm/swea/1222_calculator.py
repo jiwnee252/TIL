1b0baf7dea81985d132f03349f90a70748db8039
jiwon <jiwnee252@gmail.com>
2025-02-20 16:49:29 +0900

    4871

--- python/python-algorithm/swea/1222_calculator.py
+++ python/python-algorithm/swea/1222_calculator.py
@@ -1,14 +1,26 @@
-T = int(input())
-for test_case in range(1, 4):
+import sys
+sys.stdin = open("1222.txt", "r")
+
+for test_case in range(1, 11):
+    N = int(input())
+    tokens = input()
+    operator = {'+':1}
+    stack = []
+    result = []
+    # 후위표기법으로 변환
     # 피연산자 만나면 스택에 push
     # 연산자 만나면 필요한 만큼 피연산자를 스택에서 pop하여 연산, 연산결과를 다시 스택에 push
     # 수식이 끝나면 마지막으로 스택을 pop
 
-    tokens = input()
+    for i in range(N):
+        if tokens[i].isnumeric():
 
-    operator = {'+':1, '*':2, '/':2}
-    stack = []
-    result = []
+            
+
+
+
+
+
 
     for i in range(len(tokens)):
         if tokens[i].isnumeric():   # 숫자이면
