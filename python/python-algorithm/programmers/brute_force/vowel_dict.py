1ab97e7214d7fbdd6c3558c222f230dba918b7af
jiwon <jiwnee252@gmail.com>
2025-03-14 17:41:55 +0900

    10966

---
+++ python/python-algorithm/programmers/brute_force/vowel_dict.py
@@ -0,0 +1,11 @@
+# 길이 5이하
+# 사전에서 몇번째 단어인지 리턴.
+
+def dfs(word):
+    alpha = ['A', 'E', 'I', 'O', 'U']
+    count = 0
+    # dfs로 가능한거계속탐색하면ㅅ너 count 올리다가 필요한단어 만나면 멈춤.
+    for a in alpha:
+
+    answer = 0
+    return answer
