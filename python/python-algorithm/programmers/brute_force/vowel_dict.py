b8e47083b5b8ad618c4e83a39c6c553eacb5eb9d
jiwnee252 <jiwnee252@gmail.com>
2025-03-17 00:55:54 +0900

    모음사전

--- python/python-algorithm/programmers/brute_force/vowel_dict.py
+++ python/python-algorithm/programmers/brute_force/vowel_dict.py
@@ -1,11 +1,23 @@
-# 길이 5이하
-# 사전에서 몇번째 단어인지 리턴.
+from itertools import product
+# 가능한 모든 조합
+# 데카르트 곱..
+# 여러 개의 리스트에서 하나씩 선택하여 나올 수 있는 "모든 경우의 수(중복 포함)"생성
 
-def dfs(word):
-    alpha = ['A', 'E', 'I', 'O', 'U']
-    count = 0
-    # dfs로 가능한거계속탐색하면ㅅ너 count 올리다가 필요한단어 만나면 멈춤.
-    for a in alpha:
 
-    answer = 0
-    return answer
+def solution(word):
+    dictionary = []    # 가능한단어 저장
+    for length in range(1, 6):  # 단어길이 1~5글자
+        for item in product("AEIOU", repeat=length):
+            # 문자열 AEIOU 에서 length 길이만큼 문자를 뽑아서 만들 수 있는 모든 경우의 수 반환
+            # length = 3이면 길이 3인 모든 단어 생성..
+            # for length in range(1, 6) 이라고 했으므로 length = 1, 2, 3, 4, 5인 단어를 전부 생성.
+            dictionary.append("".join(item))   # 튜플로 반환되므로 join 해준다.
+
+    dictionary.sort()
+    position = 1
+    for w in dictionary:
+        if w == word:
+            return position
+        position += 1
+
+
