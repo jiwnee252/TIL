d8b263faf8591281786cdfd0f68640cf4d4b16b6
jiwon <jiwnee252@gmail.com>
2025-03-26 17:51:12 +0900

    game_map

---
+++ python/python-algorithm/programmers/dfs_bfs/game_map_min_distance.py
@@ -0,0 +1,12 @@
+def solution(maps):
+    N = len(maps)
+    for item in maps:
+        M = len(item)
+
+    answer = 0
+    return answer
+
+# 1 이동가능 0 이동불가
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
