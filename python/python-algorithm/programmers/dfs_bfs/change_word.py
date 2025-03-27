627694bbdcfec0d880232511df86ed9e5eebcfa5
jiwon <jiwnee252@gmail.com>
2025-03-27 16:50:02 +0900

    change_word

--- python/python-algorithm/programmers/dfs_bfs/change_word.py
+++ python/python-algorithm/programmers/dfs_bfs/change_word.py
@@ -1,5 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43163
-
+from collections import deque
 def diff(word1, word2):
     # word1과 word2가 한글자만 다른지 비교한다.
     count = 0
@@ -16,22 +16,20 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     else:
-        queue = [begin]
+        queue = deque([(begin, 0)])
         count = 0
         visited = [False] * len(words)
+
         while queue:
-
-            current_word = queue.pop(0)
-            count += 1
+            current_word, count = queue.popleft()
+            if current_word == target:
+                return count
             for i in range(len(words)):
-                if not visited[i] and diff(current_word, words[i]) == True:
-                    if words[i] == target:
-                        return count
+                if not visited[i] and diff(current_word, words[i]):
                     visited[i] = True
-                    queue.append(words[i])
+                    queue.append((words[i], count+1))
 
-
+        return count
 
 print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
 print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]))
-
--- python/python-algorithm/programmers/dfs_bfs/game_map_min_distance.py
+++ python/python-algorithm/programmers/dfs_bfs/game_map_min_distance.py
@@ -1,12 +1,28 @@
+from collections import deque
+
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+
 def solution(maps):
     N = len(maps)
-    for item in maps:
-        M = len(item)
+    M = len(maps[0])
+    queue = deque()
+    queue.append((0, 0, 1))
+    visited = [[-1] * M for _ in range(N)]
+    visited[0][0] = 1
 
-    answer = 0
-    return answer
+    while queue:
+        now_x, now_y, cnt = queue.popleft()
 
-# 1 이동가능 0 이동불가
+        if now_x == N-1 and now_y == M-1:
+            return cnt
 
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
+        for d in range(4):
+            new_x = now_x + di[d]
+            new_y = now_y + dj[d]
+
+            if 0 <= new_x < N and 0 <= new_y < M and visited[new_x][new_y] == -1 and maps[new_x][new_y] == 1:
+                queue.append((new_x, new_y, cnt + 1))
+                visited[new_x][new_y] = cnt + 1
+
+    return -1
