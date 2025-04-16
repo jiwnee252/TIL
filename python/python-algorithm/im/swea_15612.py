8ebdabbc9f72fc4b08654025b7e6cce35df31bbd
jiwon <jiwnee252@gmail.com>
2025-04-16 17:59:48 +0900

    swea_15612_solving

--- python/python-algorithm/im/swea_15612.py
+++ python/python-algorithm/im/swea_15612.py
@@ -9,16 +9,29 @@ def r(chesspan):
         for j in range(8):
             if chesspan[i][j] == 'O':
                 rook_count += 1
-                if rook_count != 8:
-                    return 'no'
-                else:
-                    # 모든 행에서도 겹치면 안되고 모든 열에서도 겹치면 안됨
-                    pass
-            return 'yes'
+    print(f'rook_count = {rook_count}')
+                #
+                # if rook_count != 8:
+                #     return 'no'
+                # else:
+                #     # 모든 행에서도 겹치면 안되고 모든 열에서도 겹치면 안됨
+                #     for row in chesspan:
+                #         if row.count(rook) != 1:
+                #             return 'no'
+                #
+                #     for row in range(8):
+                #         for col in range(8):
+                #             count = 0
+                #             if chesspan[row][col] == rook:
+                #                 count += 1
+                #                 if count != 1:
+                #                     return 'no'
+                #
+                # return 'yes'
 
 
 T= int(input())
 for test_case in range(1, T+1):
-    chesspan = [list(map(str, input())) for _ in range(8)]
-
+    chesspan = [list(input().strip()) for _ in range(8)]
+    print(chesspan)
     print(f'#{test_case} {r(chesspan)}')
